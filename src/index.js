a3392d7df2566e5a26e03dfe408640dc965f0089
Movsar <movsar1991.mb@gmail.com>
2020-12-07 16:03:34 +0300

    mmm11

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const f = (state, action) =>{
+  if (action.type === "plus"){
+    return state + 1;
+    }
+  if (action.type === "minus"){
+    if(state===0){
+      return 0;
+    }
+    return state - 1;
+    }
+  if (action.type === "reset"){
+    return 0;
+    }
+  return 10;
+}
+
+const store = createStore(f);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
